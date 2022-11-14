af9341e98125d39f95f15d171e5b978b16b7e39c
diamondrarktvo <diamondra.rktvo@gmail.com>
2022-11-14 09:01:55 +0300

    Mise en place du code au sotre  pour la configuration du redux dev tools dans chrome

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore } from 'redux';
 import { reducer } from './reducers/reducer';
 
-export const store = createStore(reducer);
+//nécessaire pour le redux dev tools extension chrome
+const reduxDevTools =
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const store = createStore(reducer, reduxDevTools);
 
 store.subscribe(() => {
    console.log('State ato amn store: ');
